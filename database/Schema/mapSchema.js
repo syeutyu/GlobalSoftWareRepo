29666ab835209451e43c53257449a63a93e7c438
syeutyu <syeuty@naver.com>
2017-08-30 19:10:31 +0900

    mapSchema

--- database/Schema/mapSchema.js
+++ database/Schema/mapSchema.js
@@ -1,35 +1,12 @@
-const router = require('express').Router();
-const request = require('request');
-const cheerio = require('cheerio');
-const logic = require('./logic');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-
-router.route('/auth/weather').post((req, res) => {
-    let lati = req.body.lati;
-    let longi = req.body.longi;
-
-    logic.search(lati, longi).then((data) => {
-        res.send(data);
-        res.end();
-    });
+const mapSchema = Schema({
+    lati: { type: String },
+    longi: { type: String },
+    name: { type: String },
+    tel: { type: String },
+    category: { type: String }
 });
 
-router.route('/auth/search').get((req, res) => {
-    let lati = req.query.lati;
-    let longi = req.query.longi;
-
-
-    logic.search().then((num) => {
-        console.log(num)
-    });
-});
-
-router.route('/auth/getSi').get((req, res) => {
-    let url = req.query.url;
-    request(url, (err, res, body) => {
-        console.log(body);
-    });
-});
-
-
-module.exports = router;
+module.exports = mapSchema;
